3a1efc082e284b093dc44321e793443de5581fc0
nilold@gmail.com <Armadillo@9090>
2020-04-11 14:55:34 -0300

    created templates and first acceptance test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_restful import Api
 from flask_jwt import JWT, JWTError
 
@@ -30,6 +30,46 @@ api.add_resource(UserRegister, '/register')
 @app.errorhandler(JWTError)
 def auth_error_handler(err):
     return jsonify({"message": "Could not authorize user. Include authorization header."}), 401
+
+
+posts = []
+
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+
+@app.route('/blog')
+def blog_page():
+    return render_template('blog.html', posts=posts)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        global posts
+
+        posts.append({
+            'title': title,
+            'content': content
+        })
+
+        return redirect(url_for('blog_page'))
+    return render_template('new_post.html')
+
+
+@app.route('/post/<string:title>')
+def see_post(title):
+    global posts
+
+    for post in posts:
+        if post['title'] == title:
+            return render_template('post.html', post=post)
+
+    return render_template('post.html', post=None)
 
 
 if __name__ == '__main__':
