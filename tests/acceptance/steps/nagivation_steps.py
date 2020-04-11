8844ebb0d331d1aa25561412b9894af1428855f8
nilold@gmail.com <Armadillo@9090>
2020-04-11 15:08:24 -0300

    acceptance test: blogpage -> homepage

--- tests/acceptance/steps/nagivation_steps.py
+++ tests/acceptance/steps/nagivation_steps.py
@@ -3,14 +3,18 @@ from selenium import webdriver
 
 use_step_matcher("re")
 
+pages = {
+    "homepage": "http://127.0.0.1:5000/",
+    "blog-page": "http://127.0.0.1:5000/blog"
+}
 
-@given("I am on the homepage")
-def step_implementation(context):
+
+@given('I am on the "(.*)"')
+def step_implementation(context, page):
     context.browser = webdriver.Chrome("/Applications/chromedriver")  # lounches a chrome window
-    context.browser.get("http://127.0.0.1:5000/")
+    context.browser.get(pages[page])
 
 
-@then("I am on the blog page")
-def step_implementation(context):
-    expected_url = "http://127.0.0.1:5000/blog"
-    assert context.browser.current_url == expected_url
+@then('I am on the "(.*)"')
+def step_implementation(context, page):
+    assert context.browser.current_url == pages[page]
